7b8e8f1964cb6992e0bf47ee5bf92c5fbf356534
Shreyansh Singh <shreyansh.pettswood@gmail.com>
2025-07-27 02:11:20 -0700

    Bug fix - add reasoning to past turns

--- src/create_sft_data.py
+++ src/create_sft_data.py
@@ -2,52 +2,63 @@ import re
 import pandas as pd
 from prompts import *
 from openai import OpenAI
+from api_key import TOGETHER_API_KEY
 from transformers import AutoTokenizer
+from dotenv import load_dotenv
 
-openai_api_key = "EMPTY"
-openai_api_base = "http://localhost:9200/v1"
+load_dotenv()
 
-client = OpenAI(
-        api_key=openai_api_key,
-        base_url=openai_api_base,
-    )
+USE_TOGETHER_MODELS = True
 
-model_name = "Qwen/Qwen3-4B"
-tokenizer = AutoTokenizer.from_pretrained(model_name)
+if USE_TOGETHER_MODELS:
+    client = OpenAI(
+            api_key=TOGETHER_API_KEY,
+            base_url="https://api.together.xyz/v1",
+        )
+else:
+    client = OpenAI(
+            api_key="EMPTY",
+            base_url="http://localhost:9200/v1",
+        )
+
+model_name = "moonshotai/Kimi-K2-Instruct"
+tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
 
 def check_answer_format(answer):
-    pattern = "<guess>(.*?)</guess>"
-    match = re.search(pattern, answer)
-    if match:
-        return match.group(1).strip()
+    pattern_guess = re.compile("<guess>(.*?)</guess>", re.DOTALL)
+    match_guess = pattern_guess.search(answer)
+    pattern_think = re.compile("<think>(.*?)</think>", re.DOTALL)
+    match_think = pattern_think.search(answer)
+    if match_guess is None or match_think is None:
+        return None, None
     else:
-        return None
+        return match_guess.group(1).strip(), match_think.group(1).strip()
 
 def get_feedback(model_answer, correct_answer):
-    model_answer_chars = list(model_answer)
-    correct_answer_chars = list(correct_answer)
+    model_answer_chars = list(map(lambda x: x.lower(), model_answer))
+    correct_answer_chars = list(map(lambda x: x.lower(), correct_answer))
     feedback_str = ""
     for i in range(len(model_answer_chars)):
         if model_answer_chars[i] == correct_answer_chars[i]:
-            feedback_str += model_answer_chars[i] + "(✓) "
+            feedback_str += model_answer_chars[i].upper() + "(✓) "
         elif model_answer_chars[i] in correct_answer_chars:
-            feedback_str += model_answer_chars[i] + "(-) "
+            feedback_str += model_answer_chars[i].upper() + "(-) "
         else:
-            feedback_str += model_answer_chars[i] + "(x) "
+            feedback_str += model_answer_chars[i].upper() + "(x) "
     return feedback_str.strip()
 
 def check_answer(turn, model_answer, correct_answer):
-    print(model_answer)
-    model_answer_extracted = check_answer_format(model_answer)
+    # print(model_answer)
+    model_answer_extracted, model_think_extracted = check_answer_format(model_answer)
     # Break entire loop after 3 retries if this happens
-    if model_answer_extracted is None:
-        return False, "Model answer is not in the correct format", "Incorrect format"
+    if model_answer_extracted is None or model_think_extracted is None:
+        return False, "Model answer is not in the correct format", "Incorrect format answer", "Incorrect format think"
     
     final_feedback_str = f"Guess {turn}: {model_answer_extracted} -> FEEDBACK: {get_feedback(model_answer_extracted, correct_answer)}"
     if model_answer_extracted == correct_answer:
-        return True, final_feedback_str, model_answer_extracted
+        return True, final_feedback_str, model_answer_extracted, model_think_extracted
     else:
-        return False, final_feedback_str, model_answer_extracted
+        return False, final_feedback_str, model_answer_extracted, model_think_extracted
 
 def get_messages(turn=1, messages=None, past_response=None, last_turn_feedback=None):
     assert (turn == 1 and messages is None) or (turn > 1 and messages is not None)
@@ -70,7 +81,7 @@ def get_messages(turn=1, messages=None, past_response=None, last_turn_feedback=N
     )
     return messages, prompt
 
-def get_response(prompt):
+def get_response(prompt, messages):
     completion = client.completions.create(
         model=model_name,
         prompt=prompt,
@@ -80,6 +91,15 @@ def get_response(prompt):
         extra_body={"top_k": 20, "min_p": 0}
     )
     return completion.choices[0].text
+
+def get_response_together(prompt, messages):
+    completion = client.chat.completions.create(
+        model=model_name,
+        messages=messages,
+        max_tokens=8192,
+        stream=False
+    )
+    return completion.choices[0].message.content
 
 if __name__ == "__main__":
     word_list = open('../data/word_list.txt').read().splitlines()
@@ -99,8 +119,11 @@ if __name__ == "__main__":
 
         print(prompt)
         print("-"*50)
-        curr_response = get_response(prompt)
-        is_correct, final_feedback_str, model_answer_extracted = check_answer(turn, curr_response, correct_answer)
+        curr_response = get_response_together(prompt, messages)
+        print(curr_response)
+        print("="*100)
+
+        is_correct, final_feedback_str, model_answer_extracted, model_think_extracted = check_answer(turn, curr_response, correct_answer)
         
         print("Model prediction: ", model_answer_extracted)
         print("Final feedback: ", final_feedback_str)
@@ -109,7 +132,7 @@ if __name__ == "__main__":
             print("Yay")
             break
 
-        past_response = curr_response
+        past_response = model_think_extracted + "\n\n" + "<guess>" + model_answer_extracted + "</guess>"
         last_turn_feedback += "\n" + final_feedback_str
         print("*"*100)
     else:
