d1a70d1e62571808b0dffcc3a5d0cc3202c5b5a0
Shreyansh Singh <shreyansh.pettswood@gmail.com>
2025-07-28 05:27:58 -0700

    Don't include word if error fixed later on

--- src/get_server_error_words.py
+++ src/get_server_error_words.py
@@ -2,11 +2,15 @@ LOG_FILE_PATH = "logs/create_sft_data_moonshotai_Kimi-K2-Instruct.log"
 
 word_errors = set()
 
+file_str = open(LOG_FILE_PATH).read()
+
 with open(LOG_FILE_PATH) as f:
     lines = f.readlines()
     for l in lines:
         if "InternalServerError encountered for word " in l:
             word_error = l.split("InternalServerError encountered for word ")[1].split(" ")[0][:-1] # get rid of full stop
+            if "Successfully processed word: " + word_error.lower() in file_str or "Successfully processed word: " + word_error.upper() in file_str:
+                continue
             word_errors.add(word_error)
 
 print(list(word_errors))
