0a6da3d70192cc459d30c4ae8ff28a057153cdc5
Shreyansh Singh <shreyansh.pettswood@gmail.com>
2025-07-28 05:25:09 -0700

    Use list instead of set

--- src/get_server_error_words.py
+++ src/get_server_error_words.py
@@ -1,13 +1,12 @@
 LOG_FILE_PATH = "logs/create_sft_data_moonshotai_Kimi-K2-Instruct.log"
 
-word_errors = []
+word_errors = set()
 
 with open(LOG_FILE_PATH) as f:
     lines = f.readlines()
     for l in lines:
         if "InternalServerError encountered for word " in l:
             word_error = l.split("InternalServerError encountered for word ")[1].split(" ")[0][:-1] # get rid of full stop
-            word_errors.append(word_error)
+            word_errors.add(word_error)
 
-
-print(word_errors)
+print(list(word_errors))
